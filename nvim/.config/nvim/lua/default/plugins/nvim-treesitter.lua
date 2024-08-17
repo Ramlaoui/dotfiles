105eaa42e2355af042a823c9f871a4d88bbe5f00
Ali Ramlaoui <ramlaoui@outlook.com>
2024-08-17 16:37:12 +0200

    nvim config

--- nvim/.config/nvim/lua/default/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/default/plugins/nvim-treesitter.lua
@@ -24,26 +24,7 @@ return {
 					enable = true,
 				},
 				-- ensure these language parsers are installed
-				ensure_installed = {
-					"json",
-					"javascript",
-					"typescript",
-					"tsx",
-					"yaml",
-					"html",
-					"css",
-					"prisma",
-					"markdown",
-					"markdown_inline",
-					"svelte",
-					"graphql",
-					"bash",
-					"lua",
-					"vim",
-					"dockerfile",
-					"gitignore",
-					"query",
-				},
+				auto_install = true,
 				incremental_selection = {
 					enable = true,
 					keymaps = {
