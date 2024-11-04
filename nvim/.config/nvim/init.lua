89802f0d332ede6ef479d268659d74f17fd50889
Ali Ramlaoui <ramlaoui@outlook.com>
2024-11-04 13:16:42 +0000

    small updates

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -22,8 +22,8 @@ local augroup = vim.api.nvim_create_augroup
 local general = augroup("General", { clear = true })
 
 vim.api.nvim_create_autocmd("BufEnter", {
-	callback = function()
-		vim.opt.formatoptions:remove({ "c", "r", "o" })
-	end,
-	desc = "Disable New Line Comment",
+    callback = function()
+        vim.opt.formatoptions:remove({ "c", "r", "o" })
+    end,
+    desc = "Disable New Line Comment",
 })
