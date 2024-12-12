28dc780eb52f56216e4c8b19208bdb30c2952ea9
Ali Ramlaoui <ramlaoui@outlook.com>
2024-12-12 16:04:54 +0100

    toggle format conform

--- nvim/.config/nvim/lua/default/plugins/lsp/conform.lua
+++ nvim/.config/nvim/lua/default/plugins/lsp/conform.lua
@@ -22,11 +22,29 @@ return {
 				python = { "ruff_fix", "ruff_format" },
 				sh = { "shfmt" },
 			},
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 2000,
-			},
+			format_on_save = function(bufnr)
+				-- Disable with a global or buffer-local variable
+				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+					return
+				end
+				return {
+					lsp_fallback = true,
+					async = false,
+					timeout_ms = 2000,
+				}
+			end,
+		})
+
+		vim.api.nvim_create_user_command("FormatToggle", function(args)
+			local is_global = not args.bang
+			if is_global then
+				vim.g.disable_autoformat = not vim.g.disable_autoformat
+			else
+				vim.b.disable_autoformat = not vim.b.disable_autoformat
+			end
+		end, {
+			desc = "Toggle autoformat-on-save",
+			bang = true,
 		})
 
 		vim.keymap.set({ "n", "v" }, "<leader>fo", function()
@@ -36,5 +54,9 @@ return {
 				timeout_ms = 2000,
 			})
 		end, { desc = "Format file or range (in visual mode)" })
+
+		vim.keymap.set({ "n", "v" }, "<leader>tf", function()
+			conform.toggle()
+		end, { desc = "Toggle autoformat-on-save" })
 	end,
 }
